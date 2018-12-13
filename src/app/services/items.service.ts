115e4be3cbb547230c3bfea09a0bf98d743886ae
EngyMohammed <33846105+EngyMohammed@users.noreply.github.com>
2018-12-13 12:48:45 +0200

    add node modules to ignore git

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import {Item} from '../Menu/item.model';
+
 
 
 @Injectable({
@@ -11,6 +11,6 @@ export class ItemsService {
 constructor(private httpClient: HttpClient) {}
  getProducts(){
     return this.httpClient
-      .get<Item[]>('https://api.myjson.com/bins/z9s2i');
+      .get<any>('https://api.myjson.com/bins/z9s2i');
   }
 }
